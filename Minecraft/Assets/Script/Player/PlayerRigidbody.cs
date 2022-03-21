44d3a859b572768b9e0c85a7c3cf3dcf530790d0
PoPo4860 <52219454+PoPo4860@users.noreply.github.com>
2022-03-22 06:34:28 +0900

    feat : PlayerRigidbody 리팩토링 fix : 블럭 모서리에 걸치면 경직되는 버그 수정

--- Minecraft/Assets/Script/Player/PlayerRigidbody.cs
+++ Minecraft/Assets/Script/Player/PlayerRigidbody.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class PlayerRigidbody : MonoBehaviour
 {
-    readonly private float playerWidth = 0.3f;
-    readonly private float playerHeight = 1.5f;
+    readonly private static float playerWidth = 0.3f;
+    readonly private static float playerHeight = 1.5f;
     private bool isGrounded = false;
     private float gravity = -9.8f;
     private float maxGravity = -9.8f;
@@ -14,6 +14,47 @@ public class PlayerRigidbody : MonoBehaviour
 
     public World world;
 
+    private Vector3[] collisionVertex = new Vector3[4]
+    {
+        new Vector3(-playerWidth, 0, +playerWidth),
+        new Vector3(-playerWidth, 0, -playerWidth),
+        new Vector3(+playerWidth, 0, +playerWidth),
+        new Vector3(+playerWidth, 0, -playerWidth)
+    };
+
+    private enum Direction { Forward, Back, Left, Right};
+    private bool CheckCollision(in Vector3 vel, in Direction di)
+    {                                  
+        if(di == Direction.Left)
+        {
+            return (world.CheckBlockSolid(transform.position + vel + collisionVertex[0]) ||
+                    world.CheckBlockSolid(transform.position + vel + collisionVertex[1]) ||
+                    world.CheckBlockSolid(transform.position + vel + collisionVertex[0] + (Vector3.up * playerHeight)) ||
+                    world.CheckBlockSolid(transform.position + vel + collisionVertex[1] + (Vector3.up * playerHeight)));
+        }
+        else if (di == Direction.Right)
+        {
+            return (world.CheckBlockSolid(transform.position + vel + collisionVertex[2]) ||
+                    world.CheckBlockSolid(transform.position + vel + collisionVertex[3]) ||
+                    world.CheckBlockSolid(transform.position + vel + collisionVertex[2] + (Vector3.up * playerHeight)) ||
+                    world.CheckBlockSolid(transform.position + vel + collisionVertex[3] + (Vector3.up * playerHeight)));
+        }
+        else if (di == Direction.Forward)
+        {
+            return (world.CheckBlockSolid(transform.position + vel + collisionVertex[0]) ||
+                    world.CheckBlockSolid(transform.position + vel + collisionVertex[2]) ||
+                    world.CheckBlockSolid(transform.position + vel + collisionVertex[0] + (Vector3.up * playerHeight)) ||
+                    world.CheckBlockSolid(transform.position + vel + collisionVertex[2] + (Vector3.up * playerHeight)));
+        }
+        else if (di == Direction.Back)
+        {
+            return (world.CheckBlockSolid(transform.position + vel + collisionVertex[1]) ||
+                    world.CheckBlockSolid(transform.position + vel + collisionVertex[3]) ||
+                    world.CheckBlockSolid(transform.position + vel + collisionVertex[1] + (Vector3.up * playerHeight)) ||
+                    world.CheckBlockSolid(transform.position + vel + collisionVertex[3] + (Vector3.up * playerHeight)));
+        }
+        return false;
+    }
     // Start is called before the first frame update
 
     private void FixedUpdate()
@@ -30,95 +71,51 @@ public class PlayerRigidbody : MonoBehaviour
     }
     private void CalculateVelocity()
     {
-        moveVelocity = CalculateMove(transform.position);
-        moveVelocity += CalculateGravity(transform.position) * Vector3.up; // 중력 적용
+        moveVelocity = CalculateMove();
+        moveVelocity += CalculateGravity() * Vector3.up; // 중력 적용
     }
-    private float CalculateGravity(Vector3 pos)
+    private float CalculateGravity()
     {
         if (gravity > maxGravity)
-        {
             gravity -= Time.fixedDeltaTime * 35;
-        }
         else if (gravity < maxGravity)
-        {
             gravity = maxGravity;
-        }
+
+
         float yVelocity = Time.fixedDeltaTime * gravity;
-        // 0.2 0.8
-        isGrounded =
-            world.CheckBlockSolid(new Vector3(pos.x - playerWidth, pos.y + yVelocity, pos.z - playerWidth)) ||
-            world.CheckBlockSolid(new Vector3(pos.x - playerWidth, pos.y + yVelocity, pos.z + playerWidth)) ||
 
-            world.CheckBlockSolid(new Vector3(pos.x + playerWidth, pos.y + yVelocity, pos.z - playerWidth)) ||
-            world.CheckBlockSolid(new Vector3(pos.x + playerWidth, pos.y + yVelocity, pos.z + playerWidth)) ||
+        // 상단 충돌
+        if (gravity > 0)
+        {
+            if(CheckCollision(new Vector3(0, yVelocity + 0.3f, 0), Direction.Left) ||
+               CheckCollision(new Vector3(0, yVelocity + 0.3f, 0), Direction.Right))
+            {
+                gravity = maxGravity;
+            }
+        }
 
-            world.CheckBlockSolid(new Vector3(pos.x + playerWidth, pos.y + yVelocity, pos.z - playerWidth)) ||
-            world.CheckBlockSolid(new Vector3(pos.x + playerWidth, pos.y + yVelocity, pos.z - playerWidth)) ||
-
-            world.CheckBlockSolid(new Vector3(pos.x + playerWidth, pos.y + yVelocity, pos.z + playerWidth)) ||
-            world.CheckBlockSolid(new Vector3(pos.x - playerWidth, pos.y + yVelocity, pos.z + playerWidth));
+        // 하단 충돌
+        isGrounded = CheckCollision(new Vector3(0, yVelocity, 0), Direction.Left) ||
+                     CheckCollision(new Vector3(0, yVelocity, 0), Direction.Right);
         return isGrounded ? 0 : yVelocity;
     }
-    private Vector3 CalculateMove(in Vector3 pos)
+    private Vector3 CalculateMove()
     {
-        float minJumpValue = (int)gameObject.transform.position.y - gameObject.transform.position.y;
-        float yVelocity = (false == isGrounded && minJumpValue >= 0.3f) ? Time.fixedDeltaTime * gravity : 0;
-        Vector3[] collisionVertex = new Vector3[8]
-        {
-            pos + new Vector3(velocity.x - playerWidth, yVelocity, -playerWidth),
-            pos + new Vector3(velocity.x - playerWidth, yVelocity, +playerWidth),
+        float yVelocity = Time.fixedDeltaTime * gravity * 1.2f;
+        if(CheckCollision(new Vector3(0, yVelocity, 0), Direction.Left) ||
+           CheckCollision(new Vector3(0, yVelocity, 0), Direction.Right))
+            yVelocity = 0;
 
-            pos + new Vector3(velocity.x + playerWidth, yVelocity, -playerWidth),
-            pos + new Vector3(velocity.x + playerWidth, yVelocity, +playerWidth),
+        if (velocity.x < 0 && CheckCollision(new Vector3(velocity.x, yVelocity, 0), Direction.Left))
+            velocity.x = 0;
+        else if (velocity.x > 0 && CheckCollision(new Vector3(velocity.x, yVelocity, 0), Direction.Right))
+            velocity.x = 0;
 
-            pos + new Vector3(-playerWidth, yVelocity, velocity.z - playerWidth),
-            pos + new Vector3(+playerWidth, yVelocity, velocity.z - playerWidth),
+        if (velocity.z < 0 && CheckCollision(new Vector3(0, yVelocity, velocity.z), Direction.Back))
+            velocity.z = 0;
+        else if (velocity.z > 0 && CheckCollision(new Vector3(0, yVelocity, velocity.z), Direction.Forward))
+            velocity.z = 0;
 
-            pos + new Vector3(-playerWidth, yVelocity, velocity.z + playerWidth),
-            pos + new Vector3(+playerWidth, yVelocity, velocity.z + playerWidth)
-
-        };
-        if (velocity.x < 0)
-        {
-            if (world.CheckBlockSolid(collisionVertex[0]) ||
-                world.CheckBlockSolid(collisionVertex[1]) ||
-                world.CheckBlockSolid(collisionVertex[0] + (Vector3.up * playerHeight)) ||
-                world.CheckBlockSolid(collisionVertex[1] + (Vector3.up * playerHeight)))
-            {
-                velocity.x = 0;
-            }
-        }
-        else if (velocity.x > 0)
-        {
-            if (world.CheckBlockSolid(collisionVertex[2]) ||
-                world.CheckBlockSolid(collisionVertex[3]) ||
-                world.CheckBlockSolid(collisionVertex[2] + (Vector3.up * playerHeight)) ||
-                world.CheckBlockSolid(collisionVertex[3] + (Vector3.up * playerHeight)))
-            {
-                velocity.x = 0;
-            }
-        }
-
-        if (velocity.z < 0)
-        {
-            if (world.CheckBlockSolid(collisionVertex[4]) ||
-                world.CheckBlockSolid(collisionVertex[5]) ||
-                world.CheckBlockSolid(collisionVertex[4] + (Vector3.up * playerHeight)) ||
-                world.CheckBlockSolid(collisionVertex[5] + (Vector3.up * playerHeight)))
-            {
-                velocity.z = 0;
-            }
-        }
-        else if (velocity.z > 0)
-        {
-            if (world.CheckBlockSolid(collisionVertex[6]) ||
-                world.CheckBlockSolid(collisionVertex[7]) ||
-                world.CheckBlockSolid(collisionVertex[6] + (Vector3.up * playerHeight)) ||
-                world.CheckBlockSolid(collisionVertex[7] + (Vector3.up * playerHeight)))
-            {
-                velocity.z = 0;
-            }
-        }
         return velocity;
     }
 }
